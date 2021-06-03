ba7790fbdebcadd1a04f78122547bc183e6956a1
Zhaofeng Li <hello@zhaofeng.li>
2021-06-03 11:45:47 -0700

    pkgs.mesa: Hand pick some drivers for RISC-V

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,4 +9,10 @@ self: super: {
     uboot = super.callPackage ./uboot { };
     ubootQemu = super.callPackage ./uboot-qemu { };
   };
+
+  mesa = if super.stdenv.hostPlatform.isRiscV then super.mesa.override {
+    driDrivers = [ "r100" "r200" "nouveau" ];
+    galliumDrivers = [ "r300" "r600" "radeonsi" "nouveau" "virgl" "swrast" ];
+    vulkanDrivers = [ "amd" "swrast" ];
+  } else super.mesa;
 }
