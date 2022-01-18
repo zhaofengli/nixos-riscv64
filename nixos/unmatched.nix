b2a002d2d55fe85d7671a078a699af8929d3b314
Zhaofeng Li <hello@zhaofeng.li>
2022-01-17 20:13:39 -0800

    unmatched: Better serial setup

--- nixos/unmatched.nix
+++ nixos/unmatched.nix
@@ -9,7 +9,12 @@
   boot.loader.generic-extlinux-compatible.enable = true;
 
   boot.kernelPackages = pkgs.riscv64.linuxPackages_unmatched;
+  boot.kernelParams = [ "console=tty0" "console=ttySIF0,115200" "earlycon=sbi" ];
   boot.initrd.kernelModules = [ "nvme" "mmc_block" "mmc_spi" "spi_sifive" "spi_nor" ];
+
+  systemd.services."serial-getty@hvc0" = {
+    enable = false;
+  };
 
   environment.systemPackages = with pkgs; [
     mtdutils
