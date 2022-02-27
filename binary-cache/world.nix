e54c77f0267bd77346bf683ab70321721414af03
Zhaofeng Li <hello@zhaofeng.li>
2022-02-26 22:45:39 -0800

    binary-cache: Add VisionFive kernel to cache

--- binary-cache/world.nix
+++ binary-cache/world.nix
@@ -57,7 +57,8 @@ in {
     vim tmux wget jq htop pfetch file
     p7zip libarchive
     fish zsh
-  ] ++ cachedLinuxPackagesFor unmatched.linuxPackages
+  ] ++ cachedLinuxPackagesFor unmatched.linuxPackages_unmatched
+    ++ cachedLinuxPackagesFor unmatched.linuxPackages_visionfive
     ++ cachedLinuxPackagesFor linuxPackages
     ++ cachedLinuxPackagesFor linuxPackages_5_15
   );
