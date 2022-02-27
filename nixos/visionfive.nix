affa64a08c4b9452d03dcc3239816496e981917d
Zhaofeng Li <hello@zhaofeng.li>
2022-02-26 17:53:54 -0800

    nixos/visionfive: Enable chain_mode for stmmac

--- nixos/visionfive.nix
+++ nixos/visionfive.nix
@@ -9,7 +9,12 @@
   boot.loader.generic-extlinux-compatible.enable = true;
 
   boot.kernelPackages = pkgs.riscv64.linuxPackages_visionfive;
-  boot.kernelParams = [ "console=tty0" "console=ttyS0,115200" "earlycon=sbi" ];
+  boot.kernelParams = [
+    "console=tty0" "console=ttyS0,115200" "earlycon=sbi"
+
+    # https://github.com/starfive-tech/linux/issues/14
+    "stmmac.chain_mode=1"
+  ];
   boot.initrd.kernelModules = [ "dw-axi-dmac-platform" "dw_mmc-pltfm" "spi-dw-mmio" ];
 
   systemd.services."serial-getty@hvc0" = {
